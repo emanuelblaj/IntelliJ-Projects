5049fcbaf8039660cc5c3fd1232e48e4c1403f24
Emanuel Blaj <emanuelblaj93@gmail.com>
2022-03-23 23:57:37 +0200

    Complex builder example

--- com/design pattern/src/Kitchen.java
+++ com/design pattern/src/Kitchen.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class Bedroom {
+public class Kitchen {
 
     private Dimension dimension;
     private int ceilingHeight;
@@ -8,19 +8,17 @@ public class Bedroom {
     private Color wallColor;
     private int numberOfWindows;
     private int numberOfDoors;
-    private boolean isDouble;
-    private boolean hasEnsuite;
+    private boolean hasDishwasher;
+    private boolean hasMicrowave;
 
-
-
-    public Bedroom(Dimension dimension, int ceilingHeight, int floorNumber, Color wallColor, int numberOfWindows, int numberOfDoors, boolean isDouble, boolean hasEnsuite) {
+    public Kitchen(Dimension dimension, int ceilingHeight, int floorNumber, Color wallColor, int numberOfWindows, int numberOfDoors, boolean isDouble, boolean hasEnsuite) {
         this.dimension = dimension;
         this.ceilingHeight = ceilingHeight;
         this.floorNumber = floorNumber;
         this.wallColor = wallColor;
         this.numberOfWindows = numberOfWindows;
         this.numberOfDoors = numberOfDoors;
-        this.isDouble = isDouble;
-        this.hasEnsuite = hasEnsuite;
+        this.hasDishwasher = isDouble;
+        this.hasMicrowave = hasEnsuite;
     }
 }
