e84d457f8c74dd801a82f80754a07a73e1784863
Emanuel Blaj <emanuelblaj93@gmail.com>
2022-05-06 18:47:43 +0300

    delete half with linkedList

--- algorithms/src/Algorithms.java
+++ algorithms/src/Algorithms.java
@@ -1,32 +1,27 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.function.IntPredicate;
 import java.util.stream.Stream;
 
 
 public class Algorithms  {
 
+    public void printBinary(int n) {
+        if(n <= 0) {
+            System.out.println();
+            return;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
 
     public static void main(String[] args) {
-
-        CustomLinkedList linkedList = new CustomLinkedList();
-
-        Node firstNode = new Node(3);
-        Node  secondNode = new Node(4);
-        Node thirdNode = new Node(5);
-        Node fourthNode = new Node(6);
-
-        linkedList.head = firstNode;
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        thirdNode.next = fourthNode;
-
-        linkedList.displayContents();
-        linkedList.deleteKthNodeFromEnd(3);
-        linkedList.displayContents();
-        linkedList.deleteBackHalf();
-        linkedList.displayContents();
 
 
  }
