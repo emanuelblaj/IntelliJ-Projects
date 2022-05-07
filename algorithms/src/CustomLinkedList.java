18b26cdd57c2f95be66797dce2b6869adae05cde
Emanuel Blaj <emanuelblaj93@gmail.com>
2022-05-07 14:02:21 +0300

    Create an algorithms who show a cycleLinkedList or not is

---
+++ algorithms/src/CustomLinkedList.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class CustomLinkedList {
+    Node head;
+    public boolean hashCycle() {
+        HashSet<Node> nodes = new HashSet<>();
+        Node current = head;
+        while (current != null) {
+            if(nodes.contains(current)) {
+                return true;
+            } else {
+                nodes.add(current);
+            }
+            current = current.next;
+        }
+        return false;
+    }
+}
