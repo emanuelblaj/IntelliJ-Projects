df1f179ce4fac929746c19d20946220c566f50e2
Emanuel Blaj <emanuelblaj93@gmail.com>
2022-06-05 20:33:24 +0300

    Update the database with JDBC, get employees from department, get the number for employees, get department and increase salaries.

---
+++ jdbc-prepared-statement/src/GetEmployeesForDepartment.java
@@ -0,0 +1,66 @@
+import java.sql.*;
+
+public class GetEmployeesForDepartment {
+
+    public static void main(String[] args)throws SQLException {
+
+        Connection myConn = null;
+        CallableStatement myStmt = null;
+        ResultSet myRs = null;
+
+        try {
+//            Get a connection to database
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","student", "student");
+            String theDepartment = "Legal";
+
+//            Prepare the stored procedure call
+            myStmt = myConn.prepareCall("{call get_employees_for_department(?)}");
+
+//            Set the paramenter
+            myStmt.setString(1, theDepartment);
+
+//            Call stored procedure
+            System.out.println("Calling stored procedure. get_employees_for_department('" +  theDepartment + "')");
+            myStmt.execute();
+            System.out.printf("Finished calling stored procedure.\n\n" );
+
+//            Get the result set
+            myRs = myStmt.getResultSet();
+
+//            Display the result set
+            display(myRs);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            close(myConn, myStmt, myRs);
+        }
+    }
+
+    private static void display(ResultSet myRs) throws SQLException {
+//        Process the result set
+        while (myRs.next()) {
+            String lastName = myRs.getString("last_name");
+            String firstName = myRs.getString("first_name");
+            double salary = myRs.getDouble("salary");
+            String department = myRs.getString("department");
+
+            System.out.printf("%s, %s, %s, %.2f\n", lastName, firstName, department, salary);
+        }
+    }
+
+    private static void close(Connection myConn, Statement myStmt, ResultSet myRs) throws SQLException {
+        if(myRs != null) {
+            myRs.close();
+        }
+        if(myStmt != null) {
+            myStmt.close();
+        }
+        if(myConn != null) {
+            myConn.close();
+        }
+    }
+
+    private static void close(Statement myStmt, ResultSet myRs)throws SQLException {
+        close(null, myStmt, myRs);
+    }
+}
