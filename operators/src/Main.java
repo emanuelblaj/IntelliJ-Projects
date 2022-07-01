ced82cffb08597cf6d2e1faa8562e73218dfad84
Emanuel Blaj <emanuelblaj93@gmail.com>
2022-07-01 21:32:28 +0300

    Use String methods, wrapper classes

--- operators/src/Main.java
+++ operators/src/Main.java
@@ -1,23 +1,72 @@
+import java.sql.Struct;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-//        Java arrays = used to store multiple values in a single variable
+//        wrapper class = provides  way to use primitive data types as reference data types
+//                        reference data types contain useful methods
+//                        can be used with collections(ex. ArraysList
 
-        Scanner scanner = new Scanner(System.in);
-        String[] cars = new String[3];
-        System.out.println("Enter name of the cars: ");
-        cars[0] = scanner.next();
-        System.out.println("Enter name of the cars: ");
-        cars[1] = scanner.next();
-        System.out.println("Enter name of the cars: ");
-        cars[2] = scanner.next();
+//        primitive     // wrapper
+//        .........     //...........
+//        boolean       // Boolean
+//        char          //Character
+//        int           //Integer
+//        double        //Double
 
-        for(int i = 0; i < cars.length; i++) {
-            System.out.println("Name of the cars are for " + i + " : " + cars [i]);
+//        autoboxing  = the automatic conversion that the Java compiler makes between the primitive
+//                      type and their corresponding object wrapper class
+//        unboxig= the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Emanuel";
+
+        if(b == '@') {
+            System.out.println("This is true");
         }
+
+
+/*
+//        String =  a reference data type that can store one or more characters
+//                  reference data types have access to useful methods
+
+        String name = "Emanuel";
+
+      */
+/*  boolean result = name.equalsIgnoreCase("emanuel");*//*
+
+//        int result = name.length();
+//        char result = name.charAt(0);
+//        int result = name.indexOf("m");
+//        boolean result = name.isEmpty();
+//        String result = name.toUpperCase();
+//        String result = name.toLowerCase();
+//        String result = name.trim();
+//        String result = name.replace('e', 'a');
+
+        System.out.println(result);
+*/
+
+////        Java arrays = used to store multiple values in a single variable
+//
+//        Scanner scanner = new Scanner(System.in);
+//        String[] cars = new String[3];
+//        System.out.println("Enter name of the cars: ");
+//        cars[0] = scanner.next();
+//        System.out.println("Enter name of the cars: ");
+//        cars[1] = scanner.next();
+//        System.out.println("Enter name of the cars: ");
+//        cars[2] = scanner.next();
+//
+//        for(int i = 0; i < cars.length; i++) {
+//            System.out.println("Name of the cars are for " + i + " : " + cars [i]);
+//        }
 
         /*
          * Logical operators = used to connect two or more expressions
