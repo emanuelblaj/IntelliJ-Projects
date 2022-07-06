f7c25f465b1943b54897aca0a6cb85f4d990fe17
Emanuel Blaj <emanuelblaj93@gmail.com>
2022-07-06 21:39:11 +0300

    Method Overloaded, Variable scope and use toString() method

---
+++ tostringmethod/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*to string () = special method that all objects inherit, that returns a string that
+        *               "textually represents" an object.
+        *               can be used both implicitly and explicitlt.*/
+
+
+        Car car = new Car();
+
+    /*    System.out.println(car.toString());*/
+        System.out.println(car);
+
+    }
+
+}
