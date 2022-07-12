a4d4a663f544450b08b38c09b2a301a5c29663bf
Emanuel Blaj <emanuelblaj93@gmail.com>
2022-07-12 21:08:58 +0300

    Abstraction and encapsulation examples

---
+++ encapsulationjava/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        encapsulation =  attributes of a class will be hidden or private,
+//                        can be accessed only through methods(getters and setters)
+//                         i should make attributes private if i don't have a reason to make them
+//                         public/protected
+
+        Car car = new Car("Chevrolet", "Camaro", 2021);
+
+
+        car.setYear(2022);
+        System.out.println(car.getMake());
+        System.out.println(car.getModel());
+        System.out.println(car.getYear());
+
+    }
+}
